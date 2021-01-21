0f7d04c8253d04de9df1e5ada51d33ca60a0f8b5
tktk7l9 <saitotakuya0719@gmail.com>
2021-01-21 23:19:07 +0900

    ユーザ入力の取得

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-// ここにコードを書きます
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')!;
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.nodeValue;
+
+    // Google API　に送信
+}
+
+
+form.addEventListener('submit', searchAddressHandler);
