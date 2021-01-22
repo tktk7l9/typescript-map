332368c093ef16852c40ecc251d27fbacae95360
tktk7l9 <saitotakuya0719@gmail.com>
2021-01-22 23:03:53 +0900

    Google Map による地図の表示

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,39 @@
+import axios from 'axios';
+
 const form = document.querySelector('form')!;
-const addressInput = document.getElementById('address')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "API_KEY"
+
+type GoogleGeocodingResponse = {
+    results: { geometry: { location: { lat: number, lng: number } } }[];
+    status: 'OK' | 'ZERO_RESULTS';
+};
 
 function searchAddressHandler(event: Event) {
     event.preventDefault();
-    const enteredAddress = addressInput.nodeValue;
+    const enteredAddress = addressInput.value;
 
-    // Google API　に送信
+    axios.get <GoogleGeocodingResponse>(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress,
+    )}&key=${GOOGLE_API_KEY}`,
+    ).then(response => {
+        if (response.data.status !== 'OK') {
+            throw new Error('座標を取得できませんでした。');
+        }
+        const coordinates = response.data.results[0].geometry.location;
+        const map = new google.maps.Map(document.getElementById("map")!, {
+            center: coordinates,
+            zoom: 16,
+        });
+        new google.maps.Marker({
+            position: coordinates, map: map
+        });
+    }).catch(err => {
+        alert(err.message);
+        console.log(err);
+    });
 }
 
 
